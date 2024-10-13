76e1b59edf7f900a96b4d65ceadf29660f37a6f2
cornzz <39997278+cornzz@users.noreply.github.com>
2024-10-13 21:09:33 +0200

    Add contacts

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,6 +1,25 @@
-<section id="contact" class="py-20">
-	<div class="pl-6">
-		<h1 class="mb-5 text-2xl">Contact</h1>
-		Coming soon...
+<section id="contact" class="flex flex-col justify-center py-20 text-center">
+	<div class="flex h-full flex-col gap-10 align-middle">
+		<h1 class="mb-4 text-2xl">Contact</h1>
+		<div>
+			<h2 class="text-xl">General Management</h2>
+			Enrico Fischer<br />
+			📞 +49 30 5213 702-26<br />
+			✉️ enrico.fischer@kdschmid.de
+		</div>
+		<hr class="w-32 m-auto"/>
+		<div>
+			<h2 class="text-xl">UK and Scandinavia</h2>
+			Spiros Chatziangelou<br />
+			📞 +44 20 7395 09-17<br />
+			✉️ spiros.chatziangelou@kdschmid.co.uk
+		</div>
+		<hr class="w-32 m-auto"/>
+		<div>
+			<h2 class="text-xl">Artist Coordination</h2>
+			Thaer Eid<br />
+			📞 +49 511 36607-76<br />
+			✉️ thaer.eid@kdschmid.de
+		</div>
 	</div>
 </section>
