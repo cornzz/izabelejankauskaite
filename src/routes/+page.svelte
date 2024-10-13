f2140950a076aabd0c711df264c319c15c60f0cd
cornzz <39997278+cornzz@users.noreply.github.com>
2024-10-14 01:06:36 +0200

    New cover photo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,23 +10,24 @@
 <svelte:window bind:scrollY />
 
 <header
-	class="fixed top-0 z-0 h-screen w-screen bg-[url('/images/home-small.webp')] bg-cover
-		   bg-[center_69%] px-4 py-2 md:bg-[size:100%_auto] lg:bg-[url('/images/home.webp')]"
+	class="fixed top-0 z-0 h-screen w-screen bg-[url('/images/home-new.jpeg')] bg-cover bg-no-repeat bg-[33%_0]"
 	style="transform: translate3d(0, calc(-0.2 * {scrollY}px), 0)"
 >
-	<nav class="animate-fadein-slow ps-2 pt-6 opacity-0 sm:ps-6 sm:pt-10">
+	<!-- animate-fadein-slow opacity-0 -->
+	<nav class="pr-6 pt-8 text-right sm:pr-12 sm:pt-12">
+		<div class="absolute w-full h-full z-[-1] bg-[radial-gradient(circle_at_100%_0,rgba(11,11,11,0.4),transparent_30%)] inset-0"></div>
 		<a
-			class="inline-block border-l-[5px] border-none border-white text-3xl
-				   font-extralight text-inherit [word-spacing:9999px] sm:pl-0 sm:text-5xl sm:[word-spacing:normal]
-				   low-aspect-ratio:border-solid low-aspect-ratio:pl-5 low-aspect-ratio:text-white"
+			class="inline-block w-full border-r-[5px] border-none border-white text-3xl font-extralight
+				   text-inherit text-white [word-spacing:9999px] sm:pr-0 sm:text-5xl sm:[word-spacing:normal]
+				   low-aspect-ratio:border-solid low-aspect-ratio:pr-5 low-aspect-ratio:text-white"
 			href="/"
 		>
 			Izabelė Jankauskaitė
 		</a>
-		<div class="flex w-fit flex-col gap-4 ps-1 pt-10 lowercase low-aspect-ratio:pt-[18vh]">
+		<div class="ml-auto flex w-fit flex-col gap-4 pr-1 pt-10 lowercase text-white low-aspect-ratio:pt-[18vh]">
 			{#each links as link}
 				<button
-					class="text-left transition-all duration-[.25s] ease-in-out hover-d:hover:translate-x-[5px]
+					class="text-right transition-all duration-[.25s] ease-in-out hover-d:hover:translate-x-[-5px]
 						   hover-d:hover:transform hover-d:hover:blur-[2px] hover-d:hover:filter"
 					on:click={() => navigation.setActive(link)}
 				>
