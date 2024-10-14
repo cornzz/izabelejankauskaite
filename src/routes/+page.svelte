88adc761cc185d850988dba1ab27091cf535cdca
cornzz <39997278+cornzz@users.noreply.github.com>
2024-10-14 02:08:11 +0200

    Add version2 cover photo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-	import { Biography, Calendar, Contact, Gallery, Navigation } from '$lib'
+	import { page } from '$app/stores'
+	import { Biography, Calendar, Contact, /*Gallery,*/ Navigation } from '$lib'
+	import { onMount } from 'svelte'
 
 	const links = ['biography', 'calendar', /*'gallery',*/ 'contact']
 
 	let scrollY: number = 0
 	let navigation: Navigation
+	let v2: boolean = false
+
+	onMount(() => v2 = $page.url.searchParams.has('2'))
 </script>
 
 <svelte:window bind:scrollY />
 
 <header
-	class="fixed top-0 z-0 h-screen w-screen bg-[url('/images/home-new.jpg')] bg-cover bg-[33%_0] bg-no-repeat"
+	class="fixed top-0 z-0 h-screen w-screen bg-[url('/images/home-new.jpg')] bg-cover bg-[33%_0] bg-no-repeat
+			{v2 ? 'version2' : ''}"
 	style="transform: translate3d(0, calc(-0.2 * {scrollY}px), 0)"
 >
 	<nav class="animate-fadein-slow pl-6 pt-8 opacity-0 sm:pl-10 sm:pt-12">
@@ -22,7 +28,7 @@
 		>
 			Izabelė Jankauskaitė
 		</a>
-		<div class="flex w-fit flex-col gap-4 pl-1 pt-10 lowercase text-white low-aspect-ratio:pt-[50vh]">
+		<div class="flex w-fit flex-col gap-4 pl-1 pt-10 lowercase text-white low-aspect-ratio:pt-[50vh] buttons">
 			{#each links as link}
 				<button
 					class="text-left transition-all duration-[.25s] ease-in-out hover-d:hover:translate-x-[5px]
@@ -47,3 +53,13 @@
 <!-- <Gallery /> -->
 
 <Contact />
+
+<style lang="postcss">
+	.version2 {
+		@apply bg-[url('/images/home-new-2.jpg')];
+
+		.buttons {
+			@apply low-aspect-ratio:pt-[37vh];
+		}
+	}
+</style>
