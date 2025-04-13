39599b181a5d7975619d054029a0e03b3462acba
cornzz <39997278+cornzz@users.noreply.github.com>
2025-04-13 17:47:44 +0200

    Clean up calendar design

--- src/lib/components/CalendarItem.svelte
+++ src/lib/components/CalendarItem.svelte
@@ -1,19 +1,35 @@
 <script lang="ts">
 	import type { Event } from '$lib/types'
+	import { slide } from 'svelte/transition'
+	import EventLink from './EventLink.svelte'
 
 	export let event: Event
+
+	let showMore: boolean = false
 </script>
 
-<div class="mb-12 last:mb-0">
+<div class="mb-9">
+	<p class="italic">{event.dates}</p>
 	<h2 class="mb-1 text-xl transition-all lg:text-2xl">{@html event.title}</h2>
-	<p class="text-lg italic">{event.dates}</p>
-	{#if event.program}
-		<p class="my-2">{@html event.program.replaceAll('\n', '<br>')}</p>
+	{#if event.program && showMore}
+		<div class="mb-4 mt-2" transition:slide>
+			<p>{@html event.program.replaceAll('\n', '<br>')}</p>
+			{#if event.link}
+				<EventLink {event} />
+			{/if}
+		</div>
 	{/if}
-	{#if event.link}
-		<a class="group mt-2" href={event.link} target="_blank">
-			<span class="underline">Tickets / More</span>
-			<span class="-ml-3 opacity-0 transition-all group-hover:-ml-1 group-hover:opacity-100"> &nbsp;→ </span>
-		</a>
+
+	{#if event.program}
+		<button on:click={() => (showMore = !showMore)} class="group block w-full text-left lg:w-auto">
+			<span class="underline">{showMore ? 'Less' : 'More'} information</span>
+			<span class="-ml-3 opacity-0 transition-all group-hover:-ml-1 group-hover:opacity-100">
+				&nbsp;
+				<span class="inline-block {showMore ? 'translate-y-[2px] rotate-180' : ''}"> ↓ </span>
+			</span>
+		</button>
+	{:else if event.link}
+		<EventLink {event} />
 	{/if}
 </div>
+<hr class="mb-9 border-[#333] opacity-25 last:hidden" />
---
+++ src/lib/components/EventLink.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Event } from '$lib/types'
+
+	export let event: Event
+</script>
+
+<a class="group my-2 block" href={event.link} target="_blank">
+    <span class="underline">Buy Tickets</span>
+    <div class="inline-flex h-5 w-[18px] items-center overflow-hidden">
+        <span class="-ml-3.5 transition-all group-hover:-ml-1"> &nbsp;→ </span>
+        <img class="ml-[2px] h-4 -rotate-45 group-hover:ml-3" src="/icons/ticket.svg" alt="Ticket Icon" />
+    </div>
+</a>
